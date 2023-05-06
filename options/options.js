9494b2fc1cb7c3ba48e8754dfdeb686b917b25e0
Crowdbotics <team@crowdbotics.com>
2023-05-06 18:56:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ent-demo-pepsi-v01-40502",
+  url: "https://ent-demo-pepsi-v01-40502.botics.co",
+  api: "https://ent-demo-pepsi-v01-40502.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
